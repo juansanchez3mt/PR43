0d3ce297facba72e3391fb6f19713d19ae7550f9
luiscristomt <luis.cristo.mt@usa.edu.co>
2022-10-10 15:35:59 -0500

    creo Controller, Entities, Repository y Service de Reservation y Score

---
+++ reto3/src/main/java/com/example/reto3/repository/crudRepository/ScoreCrudRepository.java
@@ -0,0 +1,8 @@
+package com.example.reto3.repository.crudRepository;
+
+import com.example.reto3.entities.Score;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ScoreCrudRepository extends CrudRepository<Score, Integer> {
+}
+
