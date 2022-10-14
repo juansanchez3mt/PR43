2eaf929327f2a09d2371b1db977384b61fb47810
luis <luiscristomt@usa.edu.co>
2022-10-14 00:53:46 -0500

    modificacion frony

--- reto3/src/main/resources/static/js/LibraryScript.js
+++ reto3/src/main/resources/static/js/LibraryScript.js
@@ -1,15 +1,15 @@
 $("document").ready(function (){
-    getLibrary();
+    getLibs();
 });
-function getLibrary(){
+function getLibs(){
     $.ajax({
         url: "api/Lib/all",
         type: 'GET',
         dataType: 'json',
-        success: function (library){
-            $("#library").empty();
-            for(i= 0; i< library.length; i++){
-                $("#library").append(library[i].id+ " "+ library[i].name+ " "+ library[i].target+ " "+ library[i].capacity+ " "+ library[i].description+ /*" "+ category[i].id+*/ " <button onclick='getDetailLibrary("+library[i].idLibrary+")'>Seleccionar</button><button onclick='deleteLibrary("+library[i].idLibrary+")'>Borrar</button><br>");
+        success: function (libs){
+            $("#libs").empty();
+            for(i= 0; i< libs.length; i++){
+                $("#libs").append("<option value='"+libs[i].id+"'>"+libs[i].id+ " "+ libs[i].name+ " "+ libs[i].target+ " "+ libs[i].capacity+ " "+ libs[i].description+ " "+ libs[i].category+ "</option><button onclick='getDetailLibs("+libs[i].id+")'>Seleccionar</button><button onclick='deleteLibs("+libs[i].id+")'>Borrar</button><br>");
             }
         },
         error: function (xhr, status){
@@ -17,63 +17,63 @@ function getLibrary(){
         }
     });
 }
-function getLibraryInfo(){
+function getLibsInfo(){
     let data={
-        id: $("#libraryId").val(),
-        name: $("#libraryName").val(),
-        target: $("#libraryTarget").val(),
-        capacity: $("#libraryCapacity").val(),
-        description: $("#libraryDescription").val(),
+        id: $("#libsId").val(),
+        name: $("#libsName").val(),
+        target: $("#libsTarget").val(),
+        capacity: $("#libsCapacity").val(),
+        description: $("#libsDescription").val(),
         category:{
             id: $("#category option:selected").val()
         }
     }
     return data;
 }
-function cleanLibraryInfo(){
+function cleanLibsInfo(){
     let data={
-        id: $("#libraryId").val(""),
-        name: $("#libraryName").val(""),
-        target: $("#libraryTarget").val(""),
-        capacity: $("#libraryCapacity").val(""),
-        description: $("#libraryDescription").val(""),
+        id: $("#libsId").val(""),
+        name: $("#libsName").val(""),
+        target: $("#libsTarget").val(""),
+        capacity: $("#libsCapacity").val(""),
+        description: $("#libsDescription").val(""),
         category:{
             id: $("#category option:selected").val("")
         }
     }
     return data;
 }
-function saveLibrary(){
+function saveLibs(){
     $.ajax({
         url: "api/Lib/save",
         type: 'POST',
         dataType: 'json',
         contentType: 'application/json',
-        data: JSON.stringify(getLibraryInfo()),
-        success: function (Library) {
-            getLibrary();
-            console.log(getLibraryInfo());
-            cleanLibraryInfo();
+        data: JSON.stringify(getLibsInfo()),
+        success: function (libs) {
+            getLibs();
+            console.log(getLibsInfo());
+            cleanLibsInfo();
         },
         error: function(xhr, status){
             alert('Ha sucedido un problema');
         }
     });
 }
-function getDetailLibrary(idLibrary){
+function getDetailLibs(id){
     $.ajax({
         url: "api/Lib/all",
         type: 'GET',
         dataType: 'json',
-        success: function (library){
+        success: function (libs){
             let data= {
-                id: $("#libraryId").val(library[0].id),
-                name: $("#libraryName").val(library[0].name),
-                target: $("#libraryTarget").val(library[0].target),
-                capacity: $("#libraryCapacity").val(library[0].capacity),
-                description: $("#libraryDescription").val(library[0].description),
+                id: $("#libsId").val(libs[0].id),
+                name: $("#libsName").val(libs[0].name),
+                target: $("#libsTarget").val(libs[0].target),
+                capacity: $("#libsCapacity").val(libs[0].capacity),
+                description: $("#libsDescription").val(libs[0].description),
                 category: {
-                    id: $("#category option:selected").val(category[0].category)
+                    id: $("#category option:selected").val(libs[0].category)
                 }
             }
         },
